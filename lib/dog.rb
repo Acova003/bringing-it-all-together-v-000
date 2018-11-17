f6a0c1519494d62dd2adf02d95f77ea6f3042d86
Amee Covarrubias <ameecova@yahoo.com>
2018-11-17 04:43:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
     if dog.id
-      dog_id = dog[0]
-      dog = Dog.new(dog_id[0], )
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], )
   end 
 end 
