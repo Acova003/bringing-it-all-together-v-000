db012e6e603d7b0d317e81f13337fc16e2b2af97
Amee Covarrubias <ameecova@yahoo.com>
2018-11-17 04:44:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,6 +68,6 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
     if dog.id
       dog_data = dog[0]
-      dog = Dog.new(dog_data[0], )
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
   end 
 end 
