8042e274dae5f428b0faac959ef2d085022685eb
Amee Covarrubias <ameecova@yahoo.com>
2018-11-17 04:45:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,6 +70,6 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else 
-      dog = Dog.new(id:, name:, breed:)
+      dog = Dog.new(name: name, breed: breed)
   end 
 end 
